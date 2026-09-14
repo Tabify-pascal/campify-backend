e6626b5dc32a5f2fe88ea525fb05f20bc830baa7
Tabify-pascal <pascal@tabify.org>
2026-09-14 15:05:10 +0200

    manager

--- src/services/contactService.ts
+++ src/services/contactService.ts
@@ -1,13 +1,37 @@
-import { type ContactBody } from "../schemas/contactSchema.js";
 import { prisma } from "../prisma.js";
+import type { ContactBody } from "../schemas/contactSchema.js";
+import { NotFoundError } from "../errors/NotFoundError.js";
 
 export async function createContactMessage(data: ContactBody) {
-    const contactMessage = await prisma.contactMessage.create({
-        data,
-    })
+    const camping = await prisma.camping.findUnique({
+        where: {
+            id: data.campingId,
+        },
+        select: {
+            id: true,
+        },
+    });
 
-    return {
-        success: true,
-        message: "Your message has been sent successfully",
-    };
+    if (!camping) {
+        throw new NotFoundError("Camping");
+    }
+
+    return prisma.contactMessage.create({
+        data: {
+            campingId: data.campingId,
+            name: data.name,
+            email: data.email,
+            subject: data.subject,
+            message: data.message,
+        },
+        include: {
+            camping: {
+                select: {
+                    id: true,
+                    name: true,
+                    slug: true,
+                },
+            },
+        },
+    });
 }
